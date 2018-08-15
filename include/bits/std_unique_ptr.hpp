239eb01b512ef32d676ea391c671ef44b0ad7b43
Mikhaylov Anton <anion155@gmail.com>
2018-08-15 18:49:10 +0500

    Document std::unique_ptr

--- include/bits/std_unique_ptr.hpp
+++ include/bits/std_unique_ptr.hpp
@@ -16,9 +16,37 @@
 */
 #pragma once
 
+#include <utility>
+
 namespace std {
 	template <class T> class auto_ptr;
 
+	/** @class std::unique_ptr memory
+	 * @brief std::unique_ptr is a smart pointer that owns and manages another object
+	 *   through a pointer and disposes of that object when the unique_ptr goes out of scope.
+	 * 
+	 * The object is disposed of using the associated deleter when either of the following happens:
+	 * - the managing unique_ptr object is destroyed
+	 * - the managing unique_ptr object is assigned another pointer via operator= or reset().
+	 * The object is disposed of using a potentially user-supplied deleter by calling get_deleter()(ptr).
+	 * The default deleter uses the delete operator, which destroys the object and deallocates the memory.
+	 * 
+	 * A unique_ptr may alternatively own no object, in which case it is called empty.
+	 * 
+	 * There are two versions of std::unique_ptr:
+	 * 1) Manages a single object (e.g. allocated with new)
+	 * 2) Manages a dynamically-allocated array of objects (e.g. allocated with new[])
+	 * 
+	 * The class satisfies the requirements of MoveConstructible and MoveAssignable,
+	 * but not the requirements of either CopyConstructible or CopyAssignable.
+	 * 
+	 * @tparam T - storable type
+	 * @tparam [Deleter = std::default_delete<T>] - must be FunctionObject or lvalue reference
+	 *   to a FunctionObject or lvalue reference to function, callable with an argument of
+	 *   type unique_ptr<T, Deleter>::pointer
+	 * 
+	 * @since c++11
+	 */
 	template <class T> class unique_ptr : public auto_ptr<T> {
   public:
     using auto_ptr<T>::auto_ptr;
